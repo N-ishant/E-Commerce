d27c2fa192a7fa9c16b013169c33d491c22c82b7
N-ishant <nishantupreti30@gmail.com>
2024-02-01 17:45:22 +0530

    Completed Shopping Cart

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -12,9 +12,9 @@ namespace BulkyBook.DataAccess.Repository.IRepository
 
         //T - Category
         //IEnumerable<T> GetAll();
-        IEnumerable<T> GetAll(string? includeProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
         //T Get(Expression<Func<T, bool>> filter);
-        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
