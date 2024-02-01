36d6b1ef0faa82a6de716aa8b4252806045c213e
saifiDanish <saifidanish549@gmail.com>
2024-02-01 17:52:28 +0530

    Completed service layer of Company

---
+++ BusinessAccessLayer/Interface/ICompany.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccessLayer.Interface
+{
+    public interface ICompany
+    {
+        IEnumerable<Company> GetAllCompanies();
+        Company GetCompanyById(int companyId);
+        void AddCompany(Company company);
+        void UpdateCompany(Company company);
+        bool DeleteCompany(int companyId);
+    }
+}
+}
---
+++ BusinessAccessLayer/Services/CompanyServices.cs
@@ -0,0 +1,66 @@
+﻿//using BusinessAccessLayer.Interface;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace BusinessAccessLayer.Services
+//{
+//    public class CompanyServices :ICompany
+//    {
+//    }
+//}
+
+using BusinessAccessLayer.Interface;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System.Collections.Generic;
+
+namespace BusinessAccessLayer.Services
+{
+    public class CompanyServices : ICompany
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CompanyServices(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void AddCompany(Company company)
+        {
+            _unitOfWork.Company.Add(company);
+            _unitOfWork.Save();
+        }
+
+        public bool DeleteCompany(int companyId)
+        {
+            var company = _unitOfWork.Company.GetFirstOrDefault(c => c.Id == companyId);
+            if (company != null)
+            {
+                _unitOfWork.Company.Remove(company);
+                _unitOfWork.Save();
+                return true;
+            }
+            return false;
+        }
+
+        public IEnumerable<Company> GetAllCompanies()
+        {
+            return _unitOfWork.Company.GetAll();
+        }
+
+        public Company GetCompanyById(int companyId)
+        {
+            return _unitOfWork.Company.GetFirstOrDefault(c => c.Id == companyId);
+        }
+
+        public void UpdateCompany(Company company)
+        {
+            _unitOfWork.Company.Update(company);
+            _unitOfWork.Save();
+        }
+    }
+}
+
